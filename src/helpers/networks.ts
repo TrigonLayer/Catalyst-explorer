31738ced6cf5c81d7b1bf206f3f7bc5786a5f382
Trigon Layer <160345451+TrigonLayer@users.noreply.github.com>
2025-03-18 16:01:48 +0000

    Update networks.ts

--- src/helpers/networks.ts
+++ src/helpers/networks.ts
@@ -1,32 +1,7 @@
 export default [
   {
-    name: 'Node-1',
-    url: 'http://77.68.110.194:5005/api/eth/request',
-    summary: 'Testnet node 1',
-  },
-  {
-    name: 'Node-2',
-    url: 'http://77.68.110.195:5005/api/eth/request',
-    summary: 'Testnet node 2',
-  },
-  {
-    name: 'Node-3',
-    url: 'http://77.68.110.196:5005/api/eth/request',
-    summary: 'Testnet node 3',
-  },
-  {
-    name: 'Node-4',
-    url: 'http://77.68.110.197:5005/api/eth/request',
-    summary: 'Testnet node 4',
-  },
-  {
-    name: 'Dev-Testnet',
-    url: 'http://77.68.110.78:5005/api/eth/request',
-    summary: 'Development Testnet',
-  },
-  {
-    name: 'local',
-    url: 'http://localhost:5005/api/eth/request',
-    summary: 'your local node',
+    name: 't-layer',
+    url: 'https://fraa-flashbox-4646-rpc.a.stagenet.tanssi.network',
+    summary: 'Nexus T layer',
   },
 ];
